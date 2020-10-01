2548926eb06d24437a778a2f2a4777903b1267e8
AntonPanda08 <roll2001@ukr.net>
2020-10-01 11:38:53 +0300

    Updates

---
+++ precache-manifest.4d8260fa51194597730ff4edf53349c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c761d89cc31435d34931414da7f50435",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "9b2bf4c9921ced580578",
+    "url": "/goit-react-hw-02-feedback/static/js/2.d6ce7a72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.d6ce7a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad67d8fe94c39ac2007",
+    "url": "/goit-react-hw-02-feedback/static/js/main.3fd3f597.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
